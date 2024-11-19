45e105c9ac685a597396287a5a1195e028ddfdb2
Greg Baughman <75804705+gman1647@users.noreply.github.com>
2024-11-18 21:53:16 -0500

    Add instructions and test case function calls.

---
+++ time_calculator.py
@@ -0,0 +1,33 @@
+"""
+Write a function named add_time that takes in two required parameters and one optional parameter:
+
+a start time in the 12-hour clock format (ending in AM or PM)
+a duration time that indicates the number of hours and minutes
+(optional) a starting day of the week, case insensitive
+The function should add the duration time to the start time and return the result.
+
+If the result will be the next day, it should show (next day) after the time. If the result will be more than one day later, it should show (n days later) after the time, where "n" is the number of days later.
+
+If the function is given the optional starting day of the week parameter, then the output should display the day of the week of the result. The day of the week in the output should appear after the time and before the number of days later.
+"""
+
+def add_time(start, duration):
+    pass
+
+
+
+
+    #return new_time
+
+# add_time('3:30 PM', '2:12') #should return '5:42 PM'.
+# add_time('11:55 AM', '3:12') #should return '3:07 PM'.
+# add_time('2:59 AM', '24:00') #should return '2:59 AM (next day)'.
+# add_time('11:59 PM', '24:05') # should return '12:04 AM (2 days later)'.
+# add_time('8:16 PM', '466:02') # should return '6:18 AM (20 days later)'.
+# add_time('3:30 PM', '2:12', 'Monday') #should return '5:42 PM, Monday'.
+# add_time('2:59 AM', '24:00', 'saturDay') #should return '2:59 AM, Sunday (next day)'.
+# add_time('11:59 PM', '24:05', 'Wednesday') #should return '12:04 AM, Friday (2 days later)'.
+# add_time('8:16 PM', '466:02', 'tuesday') #should return '6:18 AM, Monday (20 days later)'.
+#Waiting:3. Expected time to end with '(next day)' when it is the next day.
+#Waiting:4. Expected period to change from AM to PM at 12:00.
+#Waiting:8. Expected adding 0:00 to return the initial time.
