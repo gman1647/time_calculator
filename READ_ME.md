be2c25f530a3d907a55b3829fa85ce785e41214c
Greg Baughman <75804705+gman1647@users.noreply.github.com>
2024-11-29 00:13:48 -0500

    Updated all functions to work based of minutes. This approach seems much more streamlined. Now need to work on the days of the week calculations.

--- READ_ME.md
+++ READ_ME.md
@@ -1,24 +1,24 @@
 First thought: manipulating a dictionary seems to be the way to conquere this problem. It also seems simple while simultaneously seeming like something I will overthink.
+^This was overthinking. Doing everything in minutes was much easier.
 
 Create `split_time` function to take input in the form of hh:mm AM/PM and store it in a dictionary with keys of hour, minute, and meridian. Guess we'll find out if this is the right first step.
 
 Update `split_time` to handle duration
 
 Update `add_time` to handle very basic calculation (w/in same meridian) Meridian calculation not worked on yet, just testing the hour and minute addition. Need to work on changing meridian.
+^This approach obsolete when basing all calculations on minutes
 
 Add `meridian_cycle` to switch from AM to PM as needed.
+^update to `make_mer` Meridian now determined by minutes from midnight
 
 Add `day_checker` function to see how many days elapsed since the first time entered.
+^replaced by `make_days`
 
 I think that doing all the calcuations in minutes and then translating that back to days, hours, and minutes would work better. Added a function called `make minute` that takes a time in minutes and returns the minutes and hours.
+^This thought seems to have been correct
 
-Bugs:
-`day_checker` does not account for minutes
-hours > 12 does not account for more than 12 hours in:
-`   if minutes < 10:
-            minutes = "0" + str(minutes)
-        if hours // 12 % 2 != 0:
-            mer = meridian_cycle(mer)
-        if hours > 12:
-            hours = hours - 12
-  `
+Updated all functions to work based off of minutes. New functions include:
+`make_days`
+`make_hours`
+`make_minutes`
+`make_mer`
--- time_calculator.py
+++ time_calculator.py
@@ -16,12 +16,99 @@ new_time = {"hour": "", "minute": "","meridian":""}
 def add_time(start, duration):
     start_dict = split_time(start)
     duration = split_time(duration)
-    hours = int(start_dict["hour"]) + int(duration["hour"])
-    minutes = int(start_dict["minute"]) + int(duration["minute"])
-    minutes = int(minutes) + (int(hours) * 60)
-    print(minutes)
-    days = day_checker(start_dict["hour"], duration["hour"], start_dict["meridian"])
+    mins_start = time_to_mins(start_dict)
+    mins_dur = time_to_mins(duration)
     mer = start_dict["meridian"]
+    if mer == "PM":
+        mins_start = mins_start + 720
+    total_mins = mins_dur + mins_start
+    ret_days = make_days(total_mins)
+    ret_hours = make_hours(total_mins)
+    ret_min = make_minutes(total_mins)
+    ret_mer = make_mer(total_mins)
+    print(f'The time is now {ret_hours}:{ret_min} {ret_mer}{ret_days}')
+    return str(ret_hours) + ':' + str(ret_min) + ' ' + ret_mer + ret_days
+
+def split_time(time):
+    hour = time.split(":").pop(0)
+    minute = time.split(" ")[0].split(":").pop()
+    meridian = ""
+    if time.split(" ")[0] == time:
+        return({"hour": hour, "minute": minute})
+    else:
+        meridian = time.rsplit(" ").pop()
+        return({"hour": hour, "minute": minute, "meridian": meridian})
+
+def make_mer(min):
+    hours = min // 60
+    if (hours // 12) % 2 == 0:
+        mer = "AM"
+    else: mer = "PM"
+    return mer
+
+def time_to_mins(dict):
+    time = int(dict["hour"]) * 60 + int(dict["minute"])
+    return time
+
+def make_minutes(minutes):
+    if minutes > 59:
+        minutes = minutes % 60
+    else:
+         minutes = minutes
+    if minutes < 10:
+        minutes = "0" + str(minutes)
+    return minutes
+
+def make_hours(min):
+    if min > 59:
+        hours = min // 60
+    if hours >= 24:
+        if hours % 12 == 0:
+            hours = 12
+        else: 
+            hours = hours % 24
+    elif hours > 13:
+        hours = hours - 12
+    return hours
+
+def make_days(min):
+    days = ""
+    days = min // 1440
+    if days > 1:
+        days = " (" + str(days) + " days later)"
+    elif days == 1:
+        days = " (next day)"
+    else:
+        days = ""
+    return(days)
+
+add_time('3:30 PM', '2:12') #should return '5:42 PM'.
+add_time('11:55 AM', '3:12') #should return '3:07 PM'.
+add_time('2:59 AM', '24:00') #should return '2:59 AM (next day)'.
+add_time('11:59 PM', '24:05') # should return '12:04 AM (2 days later)'.
+add_time('8:16 PM', '466:02') # should return '6:18 AM (20 days later)'.
+# # add_time('3:30 PM', '2:12', 'Monday') #should return '5:42 PM, Monday'.
+# add_time('2:59 AM', '24:00', 'saturDay') #should return '2:59 AM, Sunday (next day)'.
+# add_time('11:59 PM', '24:05', 'Wednesday') #should return '12:04 AM, Friday (2 days later)'.
+# add_time('8:16 PM', '466:02', 'tuesday') #should return '6:18 AM, Monday (20 days later)'.
+#Waiting:3. Expected time to end with '(next day)' when it is the next day.
+#Waiting:4. Expected period to change from AM to PM at 12:00.
+#Waiting:8. Expected adding 0:00 to return the initial time.
+
+# add_time('3:30 PM', '2:12') #, 'Monday') #should return '5:42 PM, Monday'.
+# add_time('2:59 AM', '24:00')#, 'saturDay') #should return '2:59 AM, Sunday (next day)'.
+# add_time('11:59 PM', '24:05')#, 'Wednesday') #should return '12:04 AM, Friday (2 days later)'.
+# add_time('8:16 PM', '466:02')
+
+
+
+
+    # hours = int(start_dict["hour"]) + int(duration["hour"])
+    # minutes = int(start_dict["minute"]) + int(duration["minute"])
+    # minutes = int(minutes) + (int(hours) * 60)
+    # print(minutes)
+    # days = day_checker(start_dict["hour"], duration["hour"], start_dict["meridian"])
+    # mer = start_dict["meridian"]
     # if minutes > 59:
     #     hours = hours + (minutes // 60)
     #     minutes = minutes % 60
@@ -41,71 +128,3 @@ def add_time(start, duration):
     #     days = " (next day)"
     # else:
     #     days = "(" + str(days) + " days later)"
-    new_time = str(hours) + ":" + str(minutes) + " " + mer + days
-    print(new_time)
-    return new_time
-
-def split_time(time):
-    hour = time.split(":").pop(0)
-    minute = time.split(" ")[0].split(":").pop()
-    meridian = ""
-    if time.split(" ")[0] == time:
-        return({"hour": hour, "minute": minute})
-    else:
-        meridian = time.rsplit(" ").pop()
-        return({"hour": hour, "minute": minute, "meridian": meridian})
-
-def meridian_cycle(mer):
-    mer = "AM" if mer == "PM" else "PM"
-    return mer
-
-def day_checker(start, duration, mer):
-    start = int(start) + 12 if mer == "PM" else start
-    days = (int(start) + int(duration))//24
-    return(days)
-
-def make_minutes(minutes):
-    hours = ""
-    if minutes > 59:
-        hours = minutes // 60
-        minutes = minutes % 60
-    else:
-         minutes = minutes
-    if minutes < 10:
-        minutes = "0" + str(minutes)
-    return [minutes, hours]
-
-print(make_minutes(125))
-
-# def make_hours(minutes):
-# if hours // 12 % 2 != 0:
-#             mer = meridian_cycle(mer)
-#     if hours > 24:
-#         hours = hours - (24 * days)
-#     elif hours > 12:
-#         hours = hours -12
-# def make_days(hours):
-#     if days == 0:
-#         days = ""
-#     elif days == 1:
-#         days = " (next day)"
-#     else:
-#         days = "(" + str(days) + " days later)"
-
-# add_time('3:30 PM', '2:12') #should return '5:42 PM'.
-# add_time('11:55 AM', '3:12') #should return '3:07 PM'.
-# add_time('2:59 AM', '24:00') #should return '2:59 AM (next day)'.
-# add_time('11:59 PM', '24:05') # should return '12:04 AM (2 days later)'.
-# add_time('8:16 PM', '466:02') # should return '6:18 AM (20 days later)'.
-# # add_time('3:30 PM', '2:12', 'Monday') #should return '5:42 PM, Monday'.
-# add_time('2:59 AM', '24:00', 'saturDay') #should return '2:59 AM, Sunday (next day)'.
-# add_time('11:59 PM', '24:05', 'Wednesday') #should return '12:04 AM, Friday (2 days later)'.
-# add_time('8:16 PM', '466:02', 'tuesday') #should return '6:18 AM, Monday (20 days later)'.
-#Waiting:3. Expected time to end with '(next day)' when it is the next day.
-#Waiting:4. Expected period to change from AM to PM at 12:00.
-#Waiting:8. Expected adding 0:00 to return the initial time.
-
-# add_time('3:30 PM', '2:12') #, 'Monday') #should return '5:42 PM, Monday'.
-# add_time('2:59 AM', '24:00')#, 'saturDay') #should return '2:59 AM, Sunday (next day)'.
-# add_time('11:59 PM', '24:05')#, 'Wednesday') #should return '12:04 AM, Friday (2 days later)'.
-# add_time('8:16 PM', '466:02')
